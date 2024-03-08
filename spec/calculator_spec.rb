2673259d3dc80ea34bd8d1131136393b09f1b81f
nilay <nilayanand@gmail.com>
2024-03-08 09:07:18 +0530

    Add 2nd set of tests and let it fails

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -2,6 +2,7 @@
 
 require_relative '../calculator.rb'
 RSpec.describe 'Calculator' do
+  # ==== 1st set ====
   it 'has method add' do
     expect('method').to eq(defined? add)
   end
@@ -17,4 +18,15 @@ RSpec.describe 'Calculator' do
   it 'returns sum of qomma separated number' do
     expect(add('1,5')).to eq(6)
   end
+
+  # ==== 2nd set =====
+  it 'allows handle any amount of numbers' do
+    # test with 1000 numbers
+    expect(add([*1..1000].join(','))).to be_a_kind_of(Integer)
+  end
+
+  it 'also allows new line as separator' do
+    expect(add("1\n2,3")).to eq(6)
+  end
+
 end
