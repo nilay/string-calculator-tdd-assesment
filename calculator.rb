2c308bceeb6e97d8d481d2dc7b11fabfb4b35703
nilay <nilayanand@gmail.com>
2024-03-08 09:47:51 +0530

    Allow custom delimiter and railse error if negative number is passed

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+require_relative 'invalid_number_error.rb'
 def add(numbers)
-  numbers.split(/[\n,']/).map(&:to_i).sum
+  # figure out custom delimiter if its present in string
+  delimiter = numbers.start_with?('//') ? numbers[2..(numbers.index("\n") - 1)] : nil
+
+  # convert string into array removing delimiter prefix
+  numbers_as_array = numbers.delete_prefix("//#{delimiter}\n").split(/[\n,#{delimiter}]/).map(&:to_i)
+
+  raise InvalidNumberError.new('negative numbers not allowed') if numbers_as_array.select(&:negative?).count.positive?
+
+  numbers_as_array.sum
 end
